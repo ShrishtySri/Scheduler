b651137ac7de70c2ad620e712ef9cd0d7af77e02
Shrishty Srivastava <shri.srivastava23@gmail.com>
2020-08-31 19:43:57 +0530

    second big commit repo!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import { BrowserRouter as Router, Route} from "react-router-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
+import "./style.css";
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/Exercises-list.component";
 import EditExercise from "./components/edit-exercises.component";
@@ -13,23 +13,58 @@ import Login from "./auth/login.component";
 import Register from "./auth/register.component";
 import Home from "./components/home.component";
 import UserContext from './components/context/userContext';
+import Axios from "axios";
+
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(()=>{
+    const checkLoggeIn = async() => {
+      const token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await Axios.post(
+        "http://localhost:5000/users/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if (tokenRes.data) {
+        const userRes = await Axios.get("http://localhost:5000/users/", {
+          headers: { "x-auth-token": token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+
+    checkLoggeIn();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
+        <UserContext.Provider value={{ userData, setUserData }}>
           <Navbar />
           <div className="container">
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
+              <Route path="/registry" component={Register} />
               <Route path="/dashboard" component={ExercisesList} />
               <Route path="/edit/:id" component={EditExercise} />
               <Route path="/add" component={CreateExercise} />
             </Switch>
           </div>
-       
+        </UserContext.Provider>
       </BrowserRouter>
     </>
     
--- src/auth/AuthOptions.js
+++ src/auth/AuthOptions.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import UserContext from "../../src/components/context/userContext";
 
-export default function AuthOptions(){
+export default function AuthOptions() {
+    const { userData, setUserData } = useContext(UserContext);
+  
     const history = useHistory();
-
+  
     const register = () => history.push("/registry");
     const login = () => history.push("/login");
-
-    return(
-        <div>
-            <ul className="nav navbar-nav navbar-right">
-                <li className="navbar-item">
-                    <Link onClick={login} className="nav-link">Login</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link onClick={register} className="nav-link">Register</Link>
-                </li>
-                {/* <button onClick={login}>Login</button>
-                <button onClick={register}>Register</button> */}
-            </ul>    
-        </div>
+    const logout = () => {
+      setUserData({
+        token: undefined,
+        user: undefined,
+      });
+      localStorage.setItem("auth-token", "");
+    };
+  
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        {userData.user ? (
+          <li className="navbar-item">
+            <Link onClick={logout} className="nav-link">logout</Link>
+          </li>
+  
+        ) : (
+          <>
+            <li className="navbar-item">
+              <Link onClick={login} className="nav-link">Login</Link>
+            </li>
+            <li className="navbar-item">
+              <Link onClick={register} className="nav-link">Register</Link>
+            </li>
+          </>
+        )}
+      </ul>
     );
-}
+  }
